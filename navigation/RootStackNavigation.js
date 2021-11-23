cac3de236fd225ff999aa5130e65170a778e02e5
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-23 11:16:01 +0700

    Set home screen as initial screen

--- navigation/RootStackNavigation.js
+++ navigation/RootStackNavigation.js
@@ -6,7 +6,7 @@ const Stack = createNativeStackNavigator();
 
 const RootStackNavigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName={'Home'}>
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
