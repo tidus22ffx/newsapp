578abef8f60d499f1210e10bc636a26d05f634d3
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-23 11:30:24 +0700

    Installed react-native-responsive-fontsize dependencies

--- navigation/RootStackNavigation.js
+++ navigation/RootStackNavigation.js
@@ -7,7 +7,11 @@ const Stack = createNativeStackNavigator();
 const RootStackNavigation = () => {
   return (
     <Stack.Navigator initialRouteName={'Home'}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
 };
