0bef3fdb169b1d7406717bfeb4b97a742557af5b
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-23 11:10:27 +0700

    Applied Navigator

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+export {HomeScreen};
