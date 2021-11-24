97beb11bb7b8339e4110e3d018aeea932828709f
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-24 15:37:13 +0700

    Created country button component

---
+++ src/components/CountryButton.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import PropTypes from 'prop-types';
+import {Colors} from '../themes/colors';
+import {RFValue} from 'react-native-responsive-fontsize';
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.COUNTRY_BUTTON,
+    borderRadius: 4,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonSelected: {
+    backgroundColor: Colors.SELECTED_COUNTRY_BUTTON,
+    borderRadius: 4,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontFamily: 'SourceSansProRegular',
+    fontSize: 14,
+    color: Colors.COUNTRY_BUTTON_TEXT,
+  },
+  labelSelected: {
+    fontFamily: 'SourceSansProRegular',
+    fontSize: 14,
+    color: Colors.SELECTED_COUNTRY_BUTTON_TEXT,
+  },
+});
+
+const CountryButton = ({isActive, label, onPress, height, width}) => {
+  const backgroundStyle = isActive ? styles.buttonSelected : styles.button;
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{...backgroundStyle, height: height}}>
+      <Text style={isActive ? styles.labelSelected : styles.label}>
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export {CountryButton};
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {CountryButton} from '../components/CountryButton';
 
 const HomeScreen = ({navigation}) => {
   return (
     <SafeAreaView>
-      <Text>Hello</Text>
+      <CountryButton label={'United Kingdom'} height={32} />
+      <CountryButton isActive label={'United Kingdom'} height={32} />
     </SafeAreaView>
   );
 };
