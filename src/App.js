4dc36e750e6e966822a2d3b10a397464911afdb3
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-24 23:24:04 +0700

    Updated yarn.lock

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RootStackNavigation} from './navigation/RootStackNavigation';
 
