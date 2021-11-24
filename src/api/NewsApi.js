f3007166af04aa2709cbd11fa56da5c4085fbbfd
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-24 20:34:49 +0700

    Add exception handler

--- src/api/NewsApi.js
+++ src/api/NewsApi.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {create} from 'apisauce';
+import {CLIENT_ERROR, create, NETWORK_ERROR, SERVER_ERROR, TIMEOUT_ERROR} from 'apisauce';
 
 const NewsApi = create({
   baseURL: 'https://newsapi.org/v2',
@@ -12,4 +12,19 @@ const NewsEndpoints = {
   headlines: '/top-headlines',
 };
 
-export {NewsApi, NewsEndpoints};
+const throwNetworkException = constant => {
+  switch (constant) {
+    case CLIENT_ERROR:
+      return 'Oops, something wrong happened.';
+    case SERVER_ERROR:
+      return 'Oops, something wrong happened. Please try again in a moment.';
+    case TIMEOUT_ERROR:
+      return 'Connection timed out.';
+    case NETWORK_ERROR:
+      return 'You seemed to be offline.';
+    default:
+      return;
+  }
+};
+
+export {NewsApi, NewsEndpoints, throwNetworkException};
