b0149a62838c609315b732e34bd2be4c1142f767
Muhammad Faisal <muhammad.faisal@indocyber.co.id>
2021-11-24 23:23:00 +0700

    Added detail screen

--- src/navigation/RootStackNavigation.js
+++ src/navigation/RootStackNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from '../screens/HomeScreen';
+import {DetailScreen} from '../screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,11 @@ const RootStackNavigation = () => {
       <Stack.Screen
         name="Home"
         component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={DetailScreen}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,102 @@
+import format from 'date-fns/format';
+import React, {useState} from 'react';
+import {Image, Linking, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Colors} from '../themes/colors';
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: Colors.SCREEN_BACKGROUND,
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  screenTitle: {
+    fontFamily: 'SourceSansProSemiBold',
+    fontSize: RFValue(24, 768),
+    lineHeight: RFValue(36, 768),
+    color: Colors.SCREEN_TITLE_TEXT,
+    marginBottom: 28,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 343 / 170,
+  },
+  publisherText: {
+    marginTop: 10,
+    fontFamily: 'MuktaMaheeBold',
+    fontSize: RFValue(12, 768),
+    color: Colors.ARTICLE_PUBLISHER_TEXT,
+    textAlign: 'left',
+    lineHeight: RFValue(19.94, 768),
+  },
+  titleText: {
+    marginTop: 9,
+    fontFamily: 'MuktaMaheeBold',
+    fontSize: RFValue(18, 768),
+    color: Colors.ARTICLE_TITLE_TEXT,
+    textAlign: 'left',
+    lineHeight: RFValue(29.92, 768),
+  },
+  authorText: {
+    marginTop: 14,
+    fontFamily: 'MuktaMaheeRegular',
+    fontSize: RFValue(10, 768),
+    color: Colors.ARTICLE_AUTHOR_TEXT,
+    textAlign: 'left',
+    lineHeight: RFValue(16.62, 768),
+  },
+  descriptionText: {
+    marginTop: 28,
+    fontFamily: 'MuktaMaheeRegular',
+    fontSize: RFValue(14, 768),
+    color: Colors.ARTICLE_BODY_TEXT,
+    textAlign: 'left',
+    lineHeight: RFValue(23.27, 768),
+  },
+  contentText: {
+    marginTop: 16,
+    fontFamily: 'MuktaMaheeRegular',
+    fontSize: RFValue(14, 768),
+    color: Colors.ARTICLE_BODY_TEXT,
+    textAlign: 'left',
+    lineHeight: RFValue(23.27, 768),
+  },
+  urlText: {
+    marginTop: 24,
+    fontFamily: 'MuktaMaheeRegular',
+    fontSize: RFValue(14, 768),
+    color: Colors.ARTICLE_URL,
+    textAlign: 'left',
+    lineHeight: RFValue(23.27, 768),
+  },
+});
+const DetailScreen = ({navigation, route}) => {
+  const [data, setData] = useState(route.params.data);
+  const publishDate = new Date(data.publishedAt);
+  return (
+    <SafeAreaView style={styles.screen}>
+      <Text style={styles.screenTitle}>Details</Text>
+      <Image
+        source={{uri: data.urlToImage}}
+        style={styles.image}
+        resizeMode={'cover'}
+      />
+      <Text style={styles.publisherText}>{data.source.name}</Text>
+      <Text style={styles.titleText}>{data.title}</Text>
+      <Text style={styles.authorText}>
+        {data.author}
+        {data.author ? ' | ' : ' '}
+        {format(publishDate, 'MMMM dd, yyyy')}
+      </Text>
+      <Text style={styles.descriptionText}>{data.description}</Text>
+      <Text style={styles.contentText}>{data.content}</Text>
+      <TouchableOpacity onPress={() => Linking.openURL(data.url)}>
+        <Text style={styles.urlText}>{data.url}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export {DetailScreen};
